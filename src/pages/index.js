2caca76e8a6a66c9fcd921e2c0290201021737f8
Charlotte Fereday <cfereday@thoughtworks.com>
2018-12-29 14:13:28 +0000

    Charlotte - removes blog posts from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,14 +10,13 @@ import OnlineResources from '../components/Online-Resources'
 import Layout from '../components/layout'
 import { rhythm } from '../utils/typography'
 
-class BlogIndex extends React.Component {
+class Home extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const siteDescription = get(
       this,
       'props.data.site.siteMetadata.description'
     );
-    const posts = get(this, 'props.data.allMarkdownRemark.edges')
 
     return (
       <Layout location={this.props.location}>
@@ -30,30 +29,12 @@ class BlogIndex extends React.Component {
         <Meetups />
         <Bootcamps />
         <OnlineResources />
-        {posts.map(({ node }) => {
-          const title = get(node, 'frontmatter.title') || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            </div>
-          )
-        })}
       </Layout>
     )
   }
 }
 
-export default BlogIndex
+export default Home
 
 export const pageQuery = graphql`
   query {
